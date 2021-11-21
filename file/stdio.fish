3bc38e5188d7e292a5f20ddf6a16d669f87edb55
polygon <86620608+Bayu12345677@users.noreply.github.com>
2021-11-21 12:54:04 +0700

    Add files via upload

---
+++ file/stdio.fish
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+#colorama
+
+set full set_color
+
+set black set_color black
+set green set_color -o green
+set blue set_color -o blue
+set cyan set_color -o cyan
+set magenta set_color -o magenta
+set white set_color -o white
+set clean set_color normal
+set yellow set_color -o yellow
+set red set_color -o red
